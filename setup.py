c201dad365e49caf4913a61ba37a56565c2727c7
Alexandre Wetzel <alexwtz@gmail.com>
2024-02-03 08:59:57 +0100

    fix setup and release

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ setup(
   author = 'Alexandre Wetzel',                   # Type in your name
   author_email = 'alexwtz@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/alexwtz/fastInput',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/alexwtz/fastInput/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/alexwtz/FastInput/archive/refs/tags/v0.1.tar.gz',    # I explain this later on
   keywords = ['input', 'validation', 'prompt'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'enum',
+          'collections',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
